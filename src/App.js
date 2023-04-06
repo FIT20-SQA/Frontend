17375d1da81056714c41b30fbe5215eeffb0d83f
me-code-hon-me-xe <dat.dang.it.hnu@gmail.com>
2023-04-06 08:34:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Login } from "./components/Login";
+import { Navbar } from "./components/Navbar.jsx";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <Login /> */}
+    </div>
+  );
+}
+export default App;
